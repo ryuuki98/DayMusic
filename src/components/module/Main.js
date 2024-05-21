bd3bf6b72df076359379149f8e5796787ed40c86
ryuuki98 <ryuuki98@naver.com>
2024-05-21 10:12:05 +0900

    modify: test file upload

--- src/components/module/Main.js
+++ src/components/module/Main.js
@@ -6,8 +6,29 @@ const Main = () => {
         <>
             <Box>
                 <h4>Main</h4>
-                <form method="POST" action={`http://192.168.10.178:8080/user/service`}>
+                <div id = "image-container"></div>
+                <form
+                    method="POST"
+                    action={`${process.env.REACT_APP_SERVER_URL}/user/service`}
+                    encType="multipart/form-data"
+                >
                     <input type="hidden" id="command" name="command" value="test" />
+                    <input
+                        type="file"
+                        id="file"
+                        name="file"
+                        onChange={(e) => {
+                            console.log(e.target.files);
+                            const file = e.target.files[0];
+                            const reader = new FileReader();
+                            reader.onload = (f) => {
+                                console.log(f.target);
+                                const dataUrl = f.target.result;
+                                document.getElementById("image-container").innerHTML = `<img src = ${dataUrl}>`;
+                            };
+                            reader.readAsDataURL(file);
+                        }}
+                    />
                     <input type="submit" />
                 </form>
             </Box>
