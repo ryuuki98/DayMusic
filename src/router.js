ead886aa6ab2e3468d1b9adaf37379b09a461e7e
mingikim-giv <mingikim.giv@gmail.com>
2024-05-23 11:18:45 +0900

    Add: path(/follow)

--- src/router.js
+++ src/router.js
@@ -1,6 +1,16 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Root from './components/Root';
 import Main from './components/module/Main';
+import Follow from './components/Follow';
+
+// 라우터 설계
+/*
+GET /follow/followed_list 팔로잉 리스트
+GET /follow/follower_list 팔로워 리스트
+POST /follow 팔로우 추가
+DELETE /follow 팔로우 취소
+*/
+
 const router = createBrowserRouter(
     [
         {
@@ -27,6 +37,13 @@ const router = createBrowserRouter(
                 },
             ],
         },
+        {
+            path: '/follow',
+            element: <Follow />,
+            children: [
+
+            ]
+        }
     ],
     {
         basename: '/DayMusic',
