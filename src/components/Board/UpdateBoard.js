1c8628f6e5330ca00aa1e0047cab09588f4e20ae
nooleee <wjssnfl7739@naver.com>
2024-05-31 10:07:59 +0900

    Add selectedTrack 추가

--- src/components/Board/UpdateBoard.js
+++ src/components/Board/UpdateBoard.js
@@ -7,6 +7,7 @@ import { EditIcon } from '@chakra-ui/icons';
 const UpdatePost = () => {
     const location = useLocation();
     const navigate = useNavigate();
+    const [selectedTrack, setSelectedTrack] = useState(null);
     const { boardCode } = location.state || {};
     const { currentUser } = useContext(AuthContext);
     const [contents, setContents] = useState('');
