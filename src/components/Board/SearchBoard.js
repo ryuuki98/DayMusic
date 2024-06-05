89387663d34edb8100ffc86a8e10e69ceccd18f1
ryuuki98 <ryuuki98@naver.com>
2024-06-05 16:19:09 +0900

    modify: 음악 ui 수정

--- src/components/Board/SearchBoard.js
+++ src/components/Board/SearchBoard.js
@@ -212,98 +212,115 @@ const SearchBoard = () => {
 
     return (
         <>
-        <SideRankBar />
-        <Box maxW="800px" mx="10%" p={4} bg="white" overflowY="auto" height="100vh" mr="250px">
-            {error && (
-                <Alert status="error" mb={4}>
-                    <AlertIcon />
-                    {error}
-                </Alert>
-            )}
-            <VStack spacing={4}>
-                {posts.map((post) => (
-                    <Box key={post.board_code} w="full" p={4} bg="white" boxShadow="md">
-                        <Flex alignItems="center" mb={4}>
-                            <Avatar size="md" name={post.nickname} src={post.profileImg} />
-                            <Box ml={3} cursor="pointer" onClick={() => handleNicknameClick(post.id)}>
-                                <Text fontWeight="bold">{post.nickname}</Text>
-                                <Text fontSize="sm" color="gray.500">
-                                    {new Date(post.createdAt).toLocaleString()}
-                                </Text>
-                            </Box>
-                            <Menu>
-                                <MenuButton
-                                    as={IconButton}
-                                    aria-label="Options"
-                                    icon={<BsThreeDotsVertical />}
-                                    variant="ghost"
-                                    ml="auto"
+            <SideRankBar />
+            <Box maxW="800px" mx="10%" p={4} bg="white" overflowY="auto" height="100vh" mr="250px">
+                {error && (
+                    <Alert status="error" mb={4}>
+                        <AlertIcon />
+                        {error}
+                    </Alert>
+                )}
+                <VStack spacing={4}>
+                    {posts.map((post) => (
+                        <Box key={post.board_code} w="full" p={4} bg="white" boxShadow="md">
+                            <Flex alignItems="center" mb={4}>
+                                <Avatar size="md" name={post.nickname} src={post.profileImg} />
+                                <Box ml={3} cursor="pointer" onClick={() => handleNicknameClick(post.id)}>
+                                    <Text fontWeight="bold">{post.nickname}</Text>
+                                    <Text fontSize="sm" color="gray.500">
+                                        {new Date(post.createdAt).toLocaleString()}
+                                    </Text>
+                                </Box>
+                                <Menu>
+                                    <MenuButton
+                                        as={IconButton}
+                                        aria-label="Options"
+                                        icon={<BsThreeDotsVertical />}
+                                        variant="ghost"
+                                        ml="auto"
+                                    />
+                                    <MenuList>
+                                        <MenuItem onClick={() => handleEdit(post.board_code)}>Edit</MenuItem>
+                                        <MenuItem onClick={() => handleDelete(post.board_code)}>Delete</MenuItem>
+                                    </MenuList>
+                                </Menu>
+                            </Flex>
+                            <Text mb={4} cursor="pointer" onClick={() => handlePostClick(post.board_code)}>
+                                {post.contents}
+                            </Text>
+                            {post.image_url && (
+                                <Image
+                                    borderRadius="md"
+                                    src={post.image_url}
+                                    alt="Post image"
+                                    mb={4}
+                                    boxSize="500px" // 정사각형으로 만들기 위해 크기 고정
+                                    objectFit="cover"
+                                    style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }} // 이미지를 가운데 정렬하는 CSS 스타일 적용
                                 />
-                                <MenuList>
-                                    <MenuItem onClick={() => handleEdit(post.board_code)}>Edit</MenuItem>
-                                    <MenuItem onClick={() => handleDelete(post.board_code)}>Delete</MenuItem>
-                                </MenuList>
-                            </Menu>
-                        </Flex>
-                        <Text mb={4} cursor="pointer" onClick={() => handlePostClick(post.board_code)}>
-                            {post.contents}
-                        </Text>
-                        {post.image_url && (
-                            <Image
-                                borderRadius="md"
-                                src={post.image_url}
-                                alt="Post image"
-                                mb={4}
-                                boxSize="500px" // 정사각형으로 만들기 위해 크기 고정
-                                objectFit="cover"
-                                style={{ display: 'block', marginLeft: 'auto', marginRight: 'auto' }} // 이미지를 가운데 정렬하는 CSS 스타일 적용
-                            />
-                        )}
+                            )}
 
-                        {post.music_track && (
-                            <>
-                                <Text mb={4}>노래 제목: {post.music_track}</Text>
-                                <Text mb={4}>가수: {post.music_artist}</Text>
-                                <Image src={post.music_thumbnail} />
-                                <audio controls src={post.music_preview_url} />
-                            </>
-                        )}
-                        <HStack spacing={4}>
-                            <Button
-                                flex="1"
-                                variant="ghost"
-                                leftIcon={<BiLike />}
-                                onClick={handleSubmit}
-                                value={post.board_code}
-                            >
-                                <Text key={post.board_code} id={post.board_code}>
-                                    {post.likeCount}
-                                </Text>
-                                <Box as="span" mx="2"></Box>
-                                Like
-                            </Button>
-                            <Button
-                                flex="1"
-                                variant="ghost"
-                                leftIcon={<BiChat />}
-                                onClick={() => toggleComments(post.board_code)}
-                            >
-                                Comment {post.commentCount}
-                            </Button>
-                            <Button flex="1" variant="ghost" leftIcon={<BiShare />}>
-                                Share
-                            </Button>
-                        </HStack>
-                        {showComments[post.board_code] && (
-                            <CommentList
-                                boardCode={post.board_code}
-                                onAddComment={(newComment) => handleAddComment(newComment, post.board_code)}
-                            />
-                        )}
-                    </Box>
-                ))}
-            </VStack>
-        </Box>
+                            {post.music_track && (
+                                <Box mb={3} p={3} borderWidth="1px" borderRadius="lg" width="100%" bg="purple.50">
+                                    <HStack spacing={3}>
+                                        <Image
+                                            src={post.music_thumbnail}
+                                            alt={`${post.music_track} thumbnail`}
+                                            boxSize="75px"
+                                            borderRadius="md"
+                                        />
+                                        <VStack align="start" spacing={1}>
+                                            <Text fontWeight="bold" fontSize="md">
+                                                노래 제목: {post.music_track}
+                                            </Text>
+                                            <Text fontSize="sm" color="gray.500">
+                                                가수: {post.music_artist}
+                                            </Text>
+                                            <audio
+                                                controls
+                                                src={post.music_preview_url}
+                                                style={{ width: '150px' }}
+                                            ></audio>
+                                        </VStack>
+                                    </HStack>
+                                </Box>
+                            )}
+                            <HStack spacing={4}>
+                                <Button
+                                    flex="1"
+                                    variant="ghost"
+                                    leftIcon={<BiLike />}
+                                    onClick={handleSubmit}
+                                    value={post.board_code}
+                                >
+                                    <Text key={post.board_code} id={post.board_code}>
+                                        {post.likeCount}
+                                    </Text>
+                                    <Box as="span" mx="2"></Box>
+                                    Like
+                                </Button>
+                                <Button
+                                    flex="1"
+                                    variant="ghost"
+                                    leftIcon={<BiChat />}
+                                    onClick={() => toggleComments(post.board_code)}
+                                >
+                                    Comment {post.commentCount}
+                                </Button>
+                                <Button flex="1" variant="ghost" leftIcon={<BiShare />}>
+                                    Share
+                                </Button>
+                            </HStack>
+                            {showComments[post.board_code] && (
+                                <CommentList
+                                    boardCode={post.board_code}
+                                    onAddComment={(newComment) => handleAddComment(newComment, post.board_code)}
+                                />
+                            )}
+                        </Box>
+                    ))}
+                </VStack>
+            </Box>
         </>
     );
 };
