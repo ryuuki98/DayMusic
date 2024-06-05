028eae750a8db847a3289b8156c0c46baa375ae5
mingikim-giv <mingikim.giv@gmail.com>
2024-06-05 16:24:36 +0900

    Modify: SearchBoard.js

--- src/components/Board/SearchBoard.js
+++ src/components/Board/SearchBoard.js
@@ -213,7 +213,7 @@ const SearchBoard = () => {
     return (
         <>
         <SideRankBar />
-        <Box maxW="800px" mx="10%" p={4} bg="white" overflowY="auto" height="100vh" mr="250px">
+        <Box maxW="800px" mx="10%" p={4} bg="white" height="100vh" mr="250px">
             {error && (
                 <Alert status="error" mb={4}>
                     <AlertIcon />
