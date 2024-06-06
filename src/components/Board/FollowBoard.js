eda156eae3594496b1c48939d15fe8e27ca9cdaa
mingikim-giv <mingikim.giv@gmail.com>
2024-06-06 18:52:43 +0900

    Modify: FollowBoard.js(글 지움)

--- src/components/Board/FollowBoard.js
+++ src/components/Board/FollowBoard.js
@@ -216,7 +216,6 @@ const FollowBoard = () => {
             <SideRankBar />
             <Box maxW="800px" mx="10%" p={4} bg="white" height="100vh">
                 <Heading mb={4} textColor="black">
-                    Followed Posts
                 </Heading>
                 {error && (
                     <Alert status="error" mb={4}>
